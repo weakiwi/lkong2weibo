eeac313d1b76e4bec275fac0d15d51804317110a
weakiwi <670932228@qq.com>
2015-07-03 22:45:46 +0800

    增加一个token来应对400 bad request
    双倍的token双倍的快感，下一步考虑使用代理，并支持自动更新

--- send.py
+++ send.py
@@ -4,11 +4,19 @@ import urllib2
 import urllib
 
 def main(text):
-    access_token = 'your-access-token'
+    access_token = 'access_token1'
+    access_token1='access_token2'
     post_data = urllib.urlencode({'access_token' : access_token, 'status' : text.encode('utf-8') })
     post_url = 'https://api.weibo.com/2/statuses/update.json'
-    r = urllib2.urlopen(post_url, post_data);
-    print r.read()
-
+    try:
+        urllib2.urlopen(post_url, post_data)
+    except urllib2.HTTPError, e:
+        post_data1 = urllib.urlencode({'access_token' : access_token1, 'status' : text.encode('utf-8') })
+        request.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36')
+        urllib2.urlopen(post_url, post_data1)
+        print e.code
+        print e.reason
+    print 'ok';
+        
 if __name__ == '__main__':
     main(text)
