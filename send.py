a567f83e1184666ddb8ae36402ec02099d2eabd5
weakiwi <670932228@qq.com>
2015-07-03 16:42:38 +0800

    Update send.py

--- send.py
+++ send.py
@@ -4,7 +4,7 @@ import urllib2
 import urllib
 
 def main(text):
-    access_token = '2.00JVt9uBwdV6MB2549560a41PkrEgB'
+    access_token = 'your-access-token'
     post_data = urllib.urlencode({'access_token' : access_token, 'status' : text.encode('utf-8') })
     post_url = 'https://api.weibo.com/2/statuses/update.json'
     r = urllib2.urlopen(post_url, post_data);
