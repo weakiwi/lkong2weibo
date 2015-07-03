07b23260edfa9b60da42b332d592c3b92d112d93
weakiwi <670932228@qq.com>
2015-07-03 16:04:40 +0800

    获取书评部分

---
+++ send.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import urllib2
+import urllib
+
+def main(text):
+    access_token = '2.00JVt9uBwdV6MB2549560a41PkrEgB'
+    post_data = urllib.urlencode({'access_token' : access_token, 'status' : text.encode('utf-8') })
+    post_url = 'https://api.weibo.com/2/statuses/update.json'
+    r = urllib2.urlopen(post_url, post_data);
+    print r.read()
+
+if __name__ == '__main__':
+    main(text)
