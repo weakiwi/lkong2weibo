cb4e4536e8e97382f69f3bc4a7bcd5733105217f
weakiwi <670932228@qq.com>
2015-07-03 22:19:49 +0800

    抓取网址更新
    突然发现书评获取不及时是网页的问题。下一步打算解决ip封锁的问题

--- get.py
+++ get.py
@@ -4,7 +4,7 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def main():
-    url = 'http://www.yousuu.com/comments/digest' #打开书评网页
+    url = 'http://www.yousuu.com/comments' #打开书评网页
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36"}#伪装浏览器
     request = urllib2.Request(url,headers=headers)  
     response = urllib2.urlopen(request)  
