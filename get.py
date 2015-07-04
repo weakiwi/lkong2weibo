645dda2db5821518d2ee890028102e29f6839de9
weakiwi <670932228@qq.com>
2015-07-04 08:16:41 +0800

    添加注释

--- get.py
+++ get.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup
 def main():
     url = 'http://www.yousuu.com/comments' #打开书评网页
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36"}#伪装浏览器
-    request = urllib2.Request(url,headers=headers)  
+    request = urllib2.Request(url,headers=headers)#构造请求  
     response = urllib2.urlopen(request)  
     page = response.read()
-    soup=BeautifulSoup(page)
+    soup=BeautifulSoup(page)#交给beautifulsoup处理
     tag=soup.find("div","ys-comments-message")#获取书评内容的内容
     msg=tag.get_text()
     return msg
