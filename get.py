9905a2e301b68a1a9d39ac2389383c732f4a6eee
weakiwi <670932228@qq.com>
2015-07-07 20:48:20 +0800

    异步爬虫的尝试
    处理一次网页获取两个书评和两个链接，然后微博那提交两次。（原来是获取一次发送一次），成功减少了网页抓取次数（因为抓取了只获取第一个还是比较浪费的）。但是因为对python的列表用的不6，所以代码看起来异常恶心。尽量在周末改改吧。

--- get.py
+++ get.py
@@ -12,13 +12,21 @@ def main():
     page = response.read()
     soup=BeautifulSoup(page)#交给beautifulsoup处理
     tag=soup.find("div","ys-comments-message")#获取书评内容的内容
+    tag['class']='getoverit'
+    tag1=soup.find("div","ys-comments-message")
     gradetag=soup.find("span","num2star")
     grade=gradetag.get_text()
     msg=tag.get_text()
+    msg1=tag1.get_text()
     url1=soup.find(href=re.compile('/book/'))
-    bkurl='http://www.yousuu.com'+url1.get('href')
-    xode=msg.encode('utf-8')
-    if xode>280:
-        return [xode[0:280],grade,bkurl]
-    else:
-        return [xode,grade,bkurl]
+    skurl1=url1.get('href')
+    del url1['href']
+    url2=soup.find(href=re.compile('/book/'))
+    skurl2=url2.get('href')
+    bkurl1='http://www.yousuu.com'+skurl1
+    bkurl2='http://www.yousuu.com'+skurl2
+    if msg>280:
+        msg=msg[0:280]
+    if msg1>280:
+        msg1=msg1[0:280]
+    return [msg.encode('utf-8'),msg1.encode('utf-8'),bkurl1,bkurl2]
--- index.wsgi
+++ index.wsgi
@@ -5,14 +5,19 @@ import send
 import get
 import sys
 import shorturl
+import time
     
 def app(environ,start_response):
     status = '200 OK'  
     response_headers = [('Content-type', 'text/html; charset=utf-8')]  
     start_response(status, response_headers) 
     response=get.main()
-    surl=shorturl.main(response[2])
-    msg=response[0]+surl
-    return ifkvdb.main(msg)
+    surl1=shorturl.main(response[2])
+    surl2=shorturl.main(response[3])
+    msg1=response[0]+surl1
+    msg2=response[1]+surl2
+    ifkvdb.main(msg2)
+    time.sleep(15)
+    return ifkvdb.main(msg1)
 
 application = sae.create_wsgi_app(app)
