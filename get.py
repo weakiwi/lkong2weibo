037e6c6645567014833ed3306617864a9555af35
weakiwi <670932228@qq.com>
2015-07-08 19:36:04 +0800

    长微博生成模块以及优化取得代码
    做完了才发现渣浪不能发网络图片

--- get.py
+++ get.py
@@ -3,30 +3,27 @@ import urllib
 import urllib2
 import re
 from bs4 import BeautifulSoup
+import save
 
-def main():
+def main(x):
     url = 'http://www.yousuu.com/comments' #打开书评网页
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36"}#伪装浏览器
     request = urllib2.Request(url,headers=headers)#构造请求  
     response = urllib2.urlopen(request)  
     page = response.read()
     soup=BeautifulSoup(page)#交给beautifulsoup处理
-    tag=soup.find("div","ys-comments-message")#获取书评内容的内容
-    tag['class']='getoverit'
-    tag1=soup.find("div","ys-comments-message")
-    gradetag=soup.find("span","num2star")
-    grade=gradetag.get_text()
-    msg=tag.get_text()
-    msg1=tag1.get_text()
-    url1=soup.find(href=re.compile('/book/'))
-    skurl1=url1.get('href')
-    del url1['href']
-    url2=soup.find(href=re.compile('/book/'))
-    skurl2=url2.get('href')
-    bkurl1='http://www.yousuu.com'+skurl1
-    bkurl2='http://www.yousuu.com'+skurl2
-    if msg>280:
-        msg=msg[0:280]
-    if msg1>280:
-        msg1=msg1[0:280]
-    return [msg.encode('utf-8'),msg1.encode('utf-8'),bkurl1,bkurl2]
+    msg=[]
+    url=[]
+    for i in range(x):
+        tag=soup.find("div","ys-comments-message")#获取书评内容的内容
+        i=tag.get_text()
+        tag['class']='getoverit'        
+        if i>280:
+            i=i[0:280]
+	msg.append(i)
+    for u in url:
+        nu=soup.find(href=re.compile('/book/'))
+        u='http://www.yousuu.com'+nu.get('href')
+        del nu['href']
+	url.append(u)
+    return msg+url
---
+++ picture.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+import urllib2
+import urllib
+import save
+
+def main(text):
+    post_data = urllib.urlencode({'font' :'xihei', 'text' : text })#Î¢²©ÕýÎÄ+Î¢²©±êÇ©
+    post_url = 'http://www.weishell.com/changweibo/'
+    r=urllib2.urlopen(post_url, post_data)
+    picture=eval(r.read())
+    return picture["imgurl"]
